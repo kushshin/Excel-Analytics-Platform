bbef211c7b571d38168a0feb528aab5775921693
kush shin <kshitij6388@gmail.com>
2025-07-24 14:30:52 +0530

    Allow CORS for Vercel frontend

--- backEnd/app.js
+++ backEnd/app.js
@@ -19,7 +19,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 app.use(cors(
-    { origin: true,
+    { origin: "https://your-frontend.vercel.app",
         credentials: true
     }))
 
